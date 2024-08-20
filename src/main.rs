2f5d6a0d7920a64393aa52827001d0e06c7dda78
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2024-08-20 00:06:23 -0400

    init table

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use comfy_table::Table;
+use comfy_table::presets::UTF8_FULL;
+
 fn main() {
-    println!("Hello, world!");
+    let mut table = Table::new();
+    table
+        .load_preset(UTF8_FULL)
+        .set_header(vec!["keymap", "action"])
+        .add_row(vec![
+            "cmd-space",
+            "opens raycast",
+        ])
+        .add_row(vec![
+            "cmd-q",
+            "quits fouced app",
+        ]);
+
+    println!("{table}");
 }
